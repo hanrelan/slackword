52966952f7c7a68ca30a457b501706ace5eadfb7
Rohan Relan <hanrelan@yahoo.com>
2016-03-05 20:46:02 -0800

    Automatically shutdown crossword servers after 1 hour

--- lib/slackword/server.ex
+++ lib/slackword/server.ex
@@ -15,6 +15,8 @@ defmodule With do
 end
 
 defmodule Slackword.Server do
+  @timeout 1*60*60*1000 # 1 hour
+
   use GenServer
 
   require With
@@ -54,22 +56,22 @@ defmodule Slackword.Server do
   ## Server API
 
   def init(id) do
-    {:ok, %{id: id}}
+    {:ok, %{id: id}, @timeout}
   end
 
   def handle_call({:new_crossword, _date}, _from, %{crossword: _crossword} = state) do
-    {:reply, {:error, :already_exists}, state}
+    {:reply, {:error, :already_exists}, state, @timeout}
   end
 
   def handle_call({:new_crossword, %Timex.DateTime{} = date}, _from, state) do
     crossword = ActiveCrossword.new(Slackword.Crossword.new(date))
     save_crossword(self())
-    {:reply, :ok, Map.put(state, :crossword, crossword)} 
+    {:reply, :ok, Map.put(state, :crossword, crossword), @timeout} 
   end
 
   def handle_call({:get_crossword}, _from, state) do
     With.crossword(state) do
-      {:reply, {:ok, crossword}, state}
+      {:reply, {:ok, crossword}, state, @timeout}
     end
   end
 
@@ -77,29 +79,33 @@ defmodule Slackword.Server do
     With.crossword(state) do
       case ActiveCrossword.guess_word(crossword, clue_idx, guess) do
         {:error, error} ->
-          {:reply, {:error, error}, state}
+          {:reply, {:error, error}, state, @timeout}
         {:ok, crossword} ->
           save_crossword(self())
-          {:reply, :ok, %{state | crossword: crossword}}
+          {:reply, :ok, %{state | crossword: crossword}, @timeout}
       end
     end
   end
 
   def handle_call({:load_crossword}, _from, %{crossword: _crossword} = state) do
-    {:reply, :ok, state} 
+    {:reply, :ok, state, @timeout} 
   end
 
   def handle_call({:load_crossword}, _from, %{id: id} = state) do
     case Slackword.Database.load_crossword(id) do
-      :not_found -> {:reply, {:error, :not_found}, state}
-      crossword -> {:reply, :ok, Map.put(state, :crossword, crossword)}
+      :not_found -> {:reply, {:error, :not_found}, state, @timeout}
+      crossword -> {:reply, :ok, Map.put(state, :crossword, crossword), @timeout}
     end
   end
 
   def handle_cast({:save_crossword}, %{id: id, crossword: crossword} = state) do
     # TODO(rohan): Should eventually only save the answers
     Slackword.Database.save_crossword(id, crossword)
-    {:noreply, state}
+    {:noreply, state, @timeout}
+  end
+
+  def handle_info(:timeout, state) do
+    {:stop, :normal, state}
   end
 
 end
